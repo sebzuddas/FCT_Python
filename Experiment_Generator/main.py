c36e3d41f863ce38c3297ea54732e2b624def801
Zuddas-s <email@youremail.com>
2023-01-28 23:51:38 +0000

    start on experiment generation code

--- Experiment_Generator/main.py
+++ Experiment_Generator/main.py
@@ -3,3 +3,31 @@ Main file for managing the different experiments that will be run on the FCT_Mod
 
 Sebastiano Zuddas
 """
+
+from scipy.stats.qmc import LatinHypercube
+import yaml
+
+
+dimensions = 2
+sample_number = 100
+engine = LatinHypercube(dimensions)
+sample = engine.random(sample_number)
+
+
+
+props_file_location = "/Users/sebastianozuddas/Programming/Python/FCT_Python/FCT_Model/props/test.yaml"
+
+
+with open(props_file_location) as f:
+    props_file = yaml.safe_load(f)
+
+
+
+
+
+
+
+
+
+
+print(props_file)
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 main.py props/model.yaml
+python3 FCT_Model/main.py FCT_Model/props/model.yaml
